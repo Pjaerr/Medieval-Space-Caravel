d6d9b3e69fb80984c31cf83914ef9817c874704d
Joshua Jackson <washingtonfaulconer@gmail.com>
2017-09-22 13:29:09 +0100

    Setup basic tiled water and level boundaries as well as default player with basic movement (rotational/diagonal movement not included). Everything here is very very basic and not going to be in the final game, simply mocked up quickly without much planning in order to get a prototype working to see if the gameplay is fun and as intended.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed = 1;
+    private Transform trans;
+    private Transform mainCamTransform;
+
+    void Start()
+    {
+        trans = GetComponent<Transform>();
+        mainCamTransform = Camera.main.GetComponent<Transform>();
+    }
+	void Update ()
+    {
+        ProtoMovement();
+	}
+
+    void ProtoMovement()
+    {
+        float x = 0;
+        float y = 0;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            y += movementSpeed;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            y -= movementSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            x -= movementSpeed;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            
+            x += movementSpeed;
+        }
+
+        trans.Translate(new Vector2(x, y));
+    }
+}
